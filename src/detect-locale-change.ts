e21f8c1df18c781ae5b926815ecfef73089585f9
Hasan Zohdy <hassanzohdy@gmail.com>
2022-04-05 13:46:29 +0200

    "Updated History Creation"

--- src/detect-locale-change.ts
+++ src/detect-locale-change.ts
@@ -1,5 +1,5 @@
 import { routerEvents } from "./events";
-import history from "./router-history";
+import { getHistory } from "./router-history";
 import { getLocaleCodes } from "./helpers";
 
 let currentLocale: string = "";
@@ -11,7 +11,7 @@ export default function detectLocaleCodeChange() {
   // first remove the first slash from the url
   // then split the pathname by the /
   // then get the first segment of the created array
-  let [localeCode] = history.location.pathname.replace(/^\//, "").split("/");
+  let [localeCode] = getHistory().location.pathname.replace(/^\//, "").split("/");
 
   if (getLocaleCodes().includes(localeCode)) {
     routerEvents.trigger("localeCodeChange", localeCode, currentLocale);
