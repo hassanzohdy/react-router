5887264f7712d13ca3f3d72a26a66abfd869a30c
hassanzohdy <hassanzohdy@gmail.com>
2023-11-05 13:46:24 +0200

    "Enhanced query string parser"

--- src/query-string-parsers.ts
+++ src/query-string-parsers.ts
@@ -3,57 +3,57 @@ import { ObjectType } from "./types";
 
 export function toObjectParser(query: string) {
   const vars = query.split("&");
-
   const result: ObjectType = {};
 
   for (let i = 0; i < vars.length; i++) {
     const pair = vars[i].split("=");
+    const isArrayKey = pair[0].includes("[]");
 
-    let isArrayKey = pair[0].includes("[]");
-
-    // check also if it is an array, then push to array
-    // also check if the key has [], remove it
     const key = pair[0].replace("[]", "");
     let value: string | number = pair[1];
 
     if (isNumeric(value)) {
       value = Number(value);
-    } else if (typeof value === "string") {
+    } else {
       value = decodeURIComponent(value);
     }
 
-    if (result[key]) {
-      if (Array.isArray(result[key])) {
-        result[key].push(value);
+    const keys = key.split("[");
+
+    let currentObj = result;
+    for (let j = 0; j < keys.length; j++) {
+      const currentKey = keys[j].replace("]", "");
+      if (j === keys.length - 1) {
+        if (isArrayKey) {
+          if (!currentObj[currentKey]) {
+            currentObj[currentKey] = [];
+          }
+          currentObj[currentKey].push(value);
+        } else {
+          currentObj[currentKey] = value;
+        }
       } else {
-        result[key] = [result[key], value];
-      }
-    } else {
-      if (isArrayKey) {
-        result[key] = [value];
-      } else {
-        result[key] = value;
+        if (!currentObj[currentKey]) {
+          currentObj[currentKey] = {};
+        }
+        currentObj = currentObj[currentKey];
       }
     }
   }
 
   return result;
 }
-
-export function toStringParser(
-  params: ObjectType,
-  parentKey?: string,
-): string {
+export function toStringParser(params: ObjectType, parentKey?: string): string {
   const newParams: ObjectType = { ...params };
   const queryString = Object.keys(newParams)
-    .map(key => {
+    .map((key) => {
       const value = newParams[key];
       const updatedKey = parentKey ? `${parentKey}[${key}]` : key;
 
       if (typeof value === "object" && !Array.isArray(value)) {
         return toStringParser(value, updatedKey);
       } else if (Array.isArray(value)) {
-        return value.map(v => `${updatedKey}[]=${v}`).join("&");
+        return value.map((v) => `${updatedKey}[]=${v}`).join("&");
       } else {
         return `${updatedKey}=${value}`;
       }
